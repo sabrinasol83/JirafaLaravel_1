e42bc61b6ba94a18f2758a6e9dbc54444f13cb47
carigonz <carolinagonzalez794@gmail.com>
2019-07-07 17:05:15 -0300

    creando vista profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Cart;
+use App\Product;
+use App\User;
+use Auth;
 
 class UsersController extends Controller
 {
     public function profile() {
-
+        $user = Auth::user();
+        //dd($user);
+        $vac = compact('user', $user);
+        return view('/profile', $vac);
     }
 }
